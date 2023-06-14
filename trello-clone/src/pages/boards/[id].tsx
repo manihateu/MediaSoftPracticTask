e61850bc419dfa362dff2f96cd4295c2da48194a
manihateu <nik862.nikita@mail.ru>
2023-06-14 12:28:29 +0300

    addSomeLogic

--- trello-clone/src/pages/boards/[id].tsx
+++ trello-clone/src/pages/boards/[id].tsx
@@ -43,11 +43,11 @@ const BoardPage = () => {
         <label htmlFor="desc" className="block mb-2 text-sm font-medium text-gray-900">Введи описание доски</label>
         <input type="text" id="desc" onChange={(e) => {setDesc(e.target.value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
       </div>
-      <button className="mt-4" onClick={() => {handleAddBoard(name, desc)}}>
+      <button className={board.name == null ? 'mt-2' : 'hidden'} onClick={() => {handleAddBoard(name, desc)}}>
         Добавить доску
       </button>
       <button
-        className="mt-2 text-sm text-red-500"
+        className={board.name != null ? "mt-2 text-sm text-red-500" : 'hidden'}
         onClick={() => handleRemoveBoard(board.id)}
       >
         Удалить доску
--- trello-clone/src/pages/index.tsx
+++ trello-clone/src/pages/index.tsx
@@ -12,7 +12,7 @@ const WorkspaceSelectionPage = () => {
   const handleAddWorkspace = () => {
     const newWorkspace = {
       id: Math.random().toString(),
-      name: `Новая рабочая область ${workspaces.length + 1}`,
+      name: `Новая карточка ${workspaces.length + 1}`,
     };
     dispatch(addWorkspace(newWorkspace));
   };
@@ -23,24 +23,24 @@ const WorkspaceSelectionPage = () => {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Выберите рабочую область</h1>
-      <button className="mb-4" onClick={handleAddWorkspace}>
-        Добавить рабочую область
+      <h1 className="text-2xl font-bold mb-4">Выберите карточку</h1>
+      <button className="mb-4 px-2 py-2 rounded bg-gray-200" onClick={handleAddWorkspace}>
+        Добавить карточку
       </button>
-      <ul>
+      <ul className='flex flex-row gap-x-2 gap-y-2 flex-wrap'>
       {workspaces.map((workspace) => (
-  <li key={workspace.id} className="mb-2">
-    <Link href={`/boards/${workspace.id}`}>
-      {workspace.name}
-    </Link>
-    <button
-      className="ml-2 text-sm text-red-500"
-      onClick={() => handleRemoveWorkspace(workspace.id)}
-    >
-      Удалить
-    </button>
-  </li>
-))}
+        <li key={workspace.id} className="p-12 bg-gray-200 rounded-lg">
+          <Link href={`/boards/${workspace.id}`}>
+            {workspace.name}
+          </Link>
+          <button
+            className="ml-2 text-sm text-red-500"
+            onClick={() => handleRemoveWorkspace(workspace.id)}
+          >
+            Удалить
+          </button>
+        </li>
+      ))}
       </ul>
     </div>
   );
