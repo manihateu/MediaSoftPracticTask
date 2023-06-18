4f5a1dba8b8afda7ca1b0c79ce12239815317694
manihateu <102178609+manihateu@users.noreply.github.com>
2023-06-19 00:11:12 +0300

    some fix

--- e-commerce/pages/_app.tsx
+++ e-commerce/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/index.scss'
 import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
