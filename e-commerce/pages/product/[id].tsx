d1a5c92d6e8dc065fe4352e78d8d9a051148f450
manihateu <102178609+manihateu@users.noreply.github.com>
2023-06-18 17:32:38 +0300

    addProductPage

--- e-commerce/pages/product/[id].tsx
+++ e-commerce/pages/product/[id].tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import styles from '@/styles/ProductPage.module.scss'
+export default function ProductPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [product, setProduct] = useState({});
 
-export default function() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+        setProduct(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
   return (
-    <div>page</div>
-  )
+    <div className={styles.productPage}>
+      <div className={styles.productImage}>
+        <img src={product.image} alt="Product Image" />
+      </div>
+      <div className={styles.productInfo}>
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+      </div>
+    </div>
+  );
 }
- 
