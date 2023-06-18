76ab7ea7304153324a0b068c4418b63be0938b80
manihateu <102178609+manihateu@users.noreply.github.com>
2023-06-18 23:57:35 +0300

    Typed_Apptsx

--- e-commerce/pages/_app.tsx
+++ e-commerce/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/index.scss'
- 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
